1ce9dd8013c5dfc262c304e71e07802fa22f9578
James Gray <james@james4k.com>
2014-10-26 23:08:24 -0500

    vec3: Mul func is now just scalar-vector multiplication

--- vec3/vec3.go
+++ vec3/vec3.go
@@ -18,20 +18,12 @@ func Sub(a, b [3]float32) [3]float32 {
 	}
 }
 
-func Mul(a, b [3]float32) [3]float32 {
+// Mul multiples scalar k by vector v.
+func Mul(k float32, v [3]float32) [3]float32 {
 	return [3]float32{
-		a[0] * b[0],
-		a[1] * b[1],
-		a[2] * b[2],
-	}
-}
-
-// Mul1 multiples vector a with scalar b.
-func Mul1(a [3]float32, b float32) [3]float32 {
-	return [3]float32{
-		a[0] * b,
-		a[1] * b,
-		a[2] * b,
+		k * v[0],
+		k * v[1],
+		k * v[2],
 	}
 }
 
@@ -52,5 +44,5 @@ func Length(a [3]float32) float32 {
 }
 
 func Normal(a [3]float32) [3]float32 {
-	return Mul1(a, 1/Length(a))
+	return Mul(1/Length(a), a)
 }
